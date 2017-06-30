20ed2a918ceb0d5ef5d887224863e4684299fc4d
NrupM <maryenorthrup@gmail.com>
2017-06-29 20:23:36 -0700

    finished assignment

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,26 @@
 console.log("login.js loaded");
+
+let userLogin = {
+  name: "Mary",
+  password: "dog"
+}
+
+let passwordInput;
+
+let loggedIn = false;
+
+
+//iterate through the loop so long as loggedIn = false
+//prompt user for a password and save it to a variable to be compared against our object key values for correctness.
+while(!loggedIn){
+    passwordInput = prompt("Enter password for user " + userLogin.name + ".")
+    if (passwordInput === userLogin.password){
+      alert("Correct!");
+    //which changes user from not loggedIn to loggedIn
+    loggedIn = true;
+    break;
+  } else {
+    //if user gets password wrong, the will go through the loop
+    alert("password doesn't match. try again!")
+  }
+}
--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,35 @@
 console.log("security_questions.js loaded");
+
+let securityQuestions = [
+  {
+    question: "What was your first pet's name?",
+    expectedAnswer: "Charlie"
+  },
+  {
+    question: "What is your mother's maiden name?",
+    expectedAnswer: "Evans"
+  },
+  {
+    question: "What was your first car?",
+    expectedAnswer: "Volvo"
+  }
+]
+
+let userInput = "";
+let hacker = false;
+
+//prompt user for security question answer. user's input needs to equal expected answer.
+//if user gives wrong answer, they are a hacker, and must not be able to answer any more questions.
+  for (let i=0; i < securityQuestions.length; i++){
+    userInput = prompt(securityQuestions[i].question);
+    if (userInput !== securityQuestions[i].expectedAnswer){
+      alert("wrong answer!");
+      hacker = true;
+      break;
+    }
+}
+
+//if hacker = true, console.log("Stop!")
+if (hacker === true){
+  console.log("Stop it! or else.");
+}
